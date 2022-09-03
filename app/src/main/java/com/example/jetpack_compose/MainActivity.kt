bf6af72e94b16cfc068fb9b2c2a4ae58d8899cf2
YuGyeong98 <kuk3097@naver.com>
2022-09-04 01:03:25 +0900

    비만도 계산기

--- app/src/main/java/com/example/jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose/MainActivity.kt
@@ -3,95 +3,145 @@ package com.example.jetpack_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.*
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.jetpack_compose.ui.theme.JetpackComposeTheme
-import kotlinx.coroutines.launch
+import kotlin.math.pow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HomeScreen()
+            val viewModel = viewModel<BmiViewModel>()
+            val navController = rememberNavController()
+
+            val bmi = viewModel.bmi.value
+
+            NavHost(navController = navController, startDestination = "home") {
+                composable(route = "home") {
+                    HomeScreen() { height, weight ->
+                        viewModel.bmiCalculate(height, weight)
+                        navController.navigate("result")
+                    }
+                }
+                composable(route = "result") {
+                    ResultScreen(navController, bmi = bmi)
+                }
+            }
         }
     }
 }
 
 @Composable
-fun HomeScreen(viewModel: MainViewModel = viewModel()) {
-    val text1: MutableState<String> = remember {
-        mutableStateOf("Hello World")
+fun HomeScreen(onResultClicked: (Double, Double) -> Unit) {
+    val (height, setHeight) = rememberSaveable {
+        mutableStateOf("")
     }
 
-    var text2: String by remember {
-        mutableStateOf("Hello World")
+    val (weight, setWeight) = rememberSaveable {
+        mutableStateOf("")
     }
 
-    val (text, setText) = remember {
-        mutableStateOf("Hello World")
-    }
-
-    val text3: State<String> = viewModel.liveData.observeAsState("Hello World")
-
-    Column() {
-        Text(text = "Hello World")
-        Button(onClick = {
-            text1.value = "변경" // 1. text1
-            print(text1.value)
-            text2 = "변경" // 2. text2
-            print(text2)
-            setText("변경") // 3. (text, setText)
-            viewModel.changeValue("변경") // 4. viewModel
-        }) {
-            Text(text = "클릭")
+    Scaffold(topBar = {
+        TopAppBar(title = { Text(text = "비만도 계산기") })
+    }) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            OutlinedTextField(
+                value = height,
+                onValueChange = setHeight,
+                label = { Text(text = "키") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+            OutlinedTextField(
+                value = weight,
+                onValueChange = setWeight,
+                label = { Text(text = "몸무게") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(onClick = {
+                if (height.isNotEmpty() && weight.isNotEmpty()) {
+                    onResultClicked(height.toDouble(), weight.toDouble())
+                }
+            }, modifier = Modifier.align(Alignment.End)) {
+                Text(text = "결과")
+            }
         }
-        TextField(
-            value = text,
-            onValueChange = setText
-        ) // recomposition이 일어남(글자 계속 수정하거나 입력하면 다시 화면에 그려짐)
     }
 }
 
-class MainViewModel : ViewModel() {
-    private val _value: MutableState<String> =
-        mutableStateOf("Hello World") // mutableStateOf - 읽기, 쓰기 가능
-    val value: State<String> = _value // State - 읽기만 가능
+@Composable
+fun ResultScreen(navController: NavController, bmi: Double) {
+    val text = when {
+        bmi >= 35 -> "고도 비만"
+        bmi >= 30 -> "2단계 비만"
+        bmi >= 25 -> "1단계 비만"
+        bmi >= 23 -> "과체중"
+        bmi >= 18.5 -> "정상"
+        else -> "저체중"
+    }
 
-    private val _liveData = MutableLiveData<String>()
-    val liveData: LiveData<String> = _liveData
+    val imageRes = when {
+        bmi >= 23 -> R.drawable.ic_baseline_sentiment_very_dissatisfied_24
+        bmi >= 18.5 -> R.drawable.ic_baseline_sentiment_satisfied_24
+        else -> R.drawable.ic_baseline_sentiment_dissatisfied_24
+    }
 
-    fun changeValue(value: String) {
-        _value.value = value
+    Scaffold(topBar = {
+        TopAppBar(
+            title = { Text(text = "비만도 계산기") },
+            navigationIcon = {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "home",
+                    modifier = Modifier.clickable { navController.popBackStack() })
+            })
+    }) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = text, fontSize = 30.sp)
+            Spacer(modifier = Modifier.height(50.dp))
+            Image(
+                painter = painterResource(id = imageRes),
+                contentDescription = null,
+                modifier = Modifier.size(100.dp),
+                colorFilter = ColorFilter.tint(color = Color.Black)
+            )
+        }
+    }
+}
+
+class BmiViewModel : ViewModel() {
+    private val _bmi = mutableStateOf(0.0)
+    val bmi: State<Double> = _bmi
+
+    fun bmiCalculate(height: Double, weight: Double) {
+        _bmi.value = weight / (height / 100.0).pow(2.0) // 제곱
     }
 }
