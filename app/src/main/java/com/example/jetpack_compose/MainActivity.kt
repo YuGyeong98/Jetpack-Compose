2ea079fcef9ba60a0fcee6e5160eff7a94a87790
YuGyeong98 <kuk3097@naver.com>
2022-09-03 19:59:51 +0900

    Scaffold, TextField, Button, 구조분해, SnackBar, 코루틴 스코프

--- app/src/main/java/com/example/jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose/MainActivity.kt
@@ -14,131 +14,70 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpack_compose.ui.theme.JetpackComposeTheme
+import kotlinx.coroutines.launch
 
+//class MainActivity : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContent {
+//            val textValue = remember {
+//                mutableStateOf("")
+//            }
+//
+//            Column(
+//                modifier = Modifier.fillMaxSize(),
+//                verticalArrangement = Arrangement.Center,
+//                horizontalAlignment = Alignment.CenterHorizontally
+//            ) {
+//                TextField(value = textValue.value, onValueChange = {textValue.value = it}) // 텍스트 입력
+//                Button(onClick = {}) {
+//                    Text(text = "클릭")
+//                }
+//            }
+//        }
+//    }
+//}
+
+@ExperimentalComposeUiApi // 실험 기능
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var isFavorite by rememberSaveable {
-                mutableStateOf(false)
+            val (text, setValue) = remember { // 구조 분해: 객체가 가지고 있는 여러 값을 분해해서 여러 변수에 한꺼번에 초기화
+                mutableStateOf("")
             }
-            ImageCard(
-                modifier = Modifier
-                    .fillMaxWidth(0.5f)
-                    .padding(16.dp),
-                isFavorite = isFavorite
-            ) { favorite ->
-                isFavorite = favorite
-            }
-        }
-    }
-}
 
-//@Composable
-//fun ImageCard() {
-//    val isFavorite = remember { // 상태 기억
-//        mutableStateOf(false) // isFavorite 상수는 MutableState 형식
-//    }
-//
-//    Card(
-//        modifier = Modifier
-//            .fillMaxWidth(0.5f)
-//            .padding(16.dp),
-//        shape = RoundedCornerShape(8.dp),
-//        elevation = 5.dp
-//    ) {
-//        Box(modifier = Modifier.height(200.dp)) {
-//            Image(
-//                painter = painterResource(id = R.drawable.image),
-//                contentDescription = "image",
-//                contentScale = ContentScale.Crop
-//            )
-//            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopEnd) { // 이미지 위에 좋아요 기능
-//                IconButton(onClick = { isFavorite.value = !isFavorite.value }) {
-//                    Icon(
-//                        imageVector = if (isFavorite.value) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
-//                        contentDescription = "favorite",
-//                        tint = Color.White
-//                    )
-//                }
-//            }
-//        }
-//    }
-//}
+            val scaffoldState = rememberScaffoldState()
+            val scope = rememberCoroutineScope()
+            val keyboardController = LocalSoftwareKeyboardController.current
 
-//@Composable
-//fun ImageCard() {
-//    var isFavorite by rememberSaveable { // by - value 생략 가능, rememberSaveable - 화면을 돌려도 설정 보존
-//        mutableStateOf(false) // isFavorite 변수는 Boolean 형식
-//    }
-//
-//    Card(
-//        modifier = Modifier
-//            .fillMaxWidth(0.5f)
-//            .padding(16.dp),
-//        shape = RoundedCornerShape(8.dp),
-//        elevation = 5.dp
-//    ) {
-//        Box(modifier = Modifier.height(200.dp)) {
-//            Image(
-//                painter = painterResource(id = R.drawable.image),
-//                contentDescription = "image",
-//                contentScale = ContentScale.Crop
-//            )
-//            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopEnd) { // 이미지 위에 좋아요 기능
-//                IconButton(onClick = { isFavorite = !isFavorite }) {
-//                    Icon(
-//                        imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
-//                        contentDescription = "favorite",
-//                        tint = Color.White
-//                    )
-//                }
-//            }
-//        }
-//    }
-//}
-
-@Composable
-fun ImageCard(
-    modifier: Modifier = Modifier,
-    isFavorite: Boolean,
-    onTabFavorite: (Boolean) -> Unit
-) { // 재사용 가능하도록 외부에서 들어오는 값으로 변경, 콜백 함수
-    Card(
-        modifier = modifier,
-        shape = RoundedCornerShape(8.dp),
-        elevation = 5.dp
-    ) {
-        Box(modifier = Modifier.height(200.dp)) {
-            Image(
-                painter = painterResource(id = R.drawable.image),
-                contentDescription = "image",
-                contentScale = ContentScale.Crop
-            )
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.TopEnd
-            ) {
-                IconButton(onClick = { onTabFavorite(!isFavorite) }) {
-                    Icon(
-                        imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
-                        contentDescription = "favorite",
-                        tint = Color.White
-                    )
+            Scaffold(scaffoldState = scaffoldState) { // Material Component들을 사용할 수 있게 미리 디자인된 레이아웃, snackbar 사용하려면 필요
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    TextField(value = text, onValueChange = setValue)
+                    Button(onClick = {
+                        keyboardController?.hide() // null 안전한 호출 - ? 사용
+                        scope.launch {
+                            scaffoldState.snackbarHostState.showSnackbar("Hello $text") // showSnackbar 함수는 코루틴 스코프 사용 필요
+                        }
+                    }) {
+                        Text(text = "클릭")
+                    }
                 }
             }
         }
